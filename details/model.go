01a500e3c3ee13ebad2c579ac161ec8acc565a97
Richard Garai <richard.garai.con@hotstar.com>
2023-02-03 14:31:58 +0100

    add details model json serialization

--- details/model.go
+++ details/model.go
@@ -1,16 +1,16 @@
 package details
 
 type Details struct {
-	Title       string
-	ReleaseYear string
-	Director    string
-	Actors      string
-	Country     string
-	Labels      string
-	Length      string
-	ImdbRating  string
-	ImdbLink    string
-	OtherLink   string
-	CoverImage  string
-	OtherImages []string
+	Title       string   `json:"title"`
+	ReleaseYear string   `json:"releaseYear"`
+	Director    string   `json:"director"`
+	Actors      string   `json:"actors"`
+	Country     string   `json:"country"`
+	Labels      string   `json:"labels"`
+	Length      string   `json:"length"`
+	ImdbRating  string   `json:"imdbRating"`
+	ImdbLink    string   `json:"imdbLink"`
+	OtherLink   string   `json:"otherLink"`
+	CoverImage  string   `json:"coverImage"`
+	OtherImages []string `json:"otherImages"`
 }
